3b6334f61ff6f2d8d6996f8c40c2feee08a4ab18
Muhun Kim <iam@muhun.kim>
2020-12-12 20:57:23 +0900

    Part 2

--- advent-of-code/2020/4.test.ts
+++ advent-of-code/2020/4.test.ts
@@ -1,44 +1,53 @@
 import { readFileSync } from 'fs'
 import path from 'path'
 
-import { validator, parser, part1, Passport } from './4'
+import {
+  validator as generalValidator,
+  parser,
+  part1,
+  Passport,
+  Part,
+  part2
+} from './4'
 
-const sample: Passport = {
-  ecl: 'gry',
-  pid: 860033327,
-  eyr: 2020,
-  hcl: '#fffffd',
-  byr: 1937,
-  iyr: 2017,
-  cid: 147,
-  hgt: '183cm'
-}
+describe('modules', () => {
+  const sample: Passport = {
+    ecl: 'gry',
+    pid: '860033327',
+    eyr: 2020,
+    hcl: '#fffffd',
+    byr: 1937,
+    iyr: 2017,
+    cid: '147',
+    hgt: '183cm'
+  }
 
-it('Parser', () => {
-  const input = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+  it('Parser', () => {
+    const input = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm`
 
-  expect(parser(input)).toStrictEqual(sample)
+    expect(parser(input)).toStrictEqual(sample)
+  })
+
+  describe('validator', () => {
+    const validator = generalValidator(Part.One)
+    it('모두 갖고 있음', () => {
+      const input = sample
+      expect(validator(input)).toBe(true)
+    })
+    it('국가 ID 만 없음', () => {
+      const { cid, ...input } = sample
+      expect(validator(input)).toBe(true)
+    })
+    it('국가 ID 외에 다른 정보도 없음', () => {
+      const { cid, ecl, ...input } = sample
+      expect(validator(input)).toBe(false)
+    })
+  })
 })
 
-describe('validator', () => {
-  it('모두 갖고 있음', () => {
-    const input = sample
-    expect(validator(input)).toBe(true)
-  })
-  it('국가 ID 만 없음', () => {
-    const { cid, ...input } = sample
-    expect(validator(input)).toBe(true)
-  })
-  it('국가 ID 외에 다른 정보도 없음', () => {
-    const { cid, ecl, ...input } = sample
-    expect(validator(input)).toBe(false)
-  })
-})
-
-describe('Part 1', () => {
-  it('sample case', () => {
-    const input = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+describe('문제', () => {
+  const sample = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
 
 iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
@@ -47,17 +56,63 @@ hcl:#cfa07d byr:1929
 hcl:#ae17e1 iyr:2013
 eyr:2024
 ecl:brn pid:760753108 byr:1931
-hgt:179cm
+hgt:179
 
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in`
-    expect(part1(input)).toBe(2)
-  })
-
-  it('find answer', () => {
-    const input = readFileSync(path.join(__dirname, '4.txt'), {
-      encoding: 'utf8'
+  describe('Part 1', () => {
+    it('샘플 입력', () => {
+      const input = sample
+      expect(part1(input)).toBe(2)
     })
-    expect(part1(input)).toBe(250)
+
+    it('정답', () => {
+      const input = readFileSync(path.join(__dirname, '4.txt'), {
+        encoding: 'utf8'
+      })
+      expect(part1(input)).toBe(250)
+    })
+  })
+  describe('Part 2', () => {
+    it('모두 유효함', () => {
+      const input = `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`
+      expect(part2(input)).toBe(4)
+    })
+    it('모두 유효하지 않음', () => {
+      const input = `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+`
+      expect(part2(input)).toBe(0)
+    })
+
+    it('정답', () => {
+      const input = readFileSync(path.join(__dirname, '4.txt'), {
+        encoding: 'utf8'
+      })
+
+      expect(part2(input)).toBe(158)
+    })
   })
 })
--- advent-of-code/2020/4.ts
+++ advent-of-code/2020/4.ts
@@ -5,27 +5,60 @@ export interface Passport {
   hgt: string
   hcl: string
   ecl: string
-  pid: number
-  cid: number
+  pid: string
+  cid: string
 }
 
-export function part1(input: string) {
+export enum Part {
+  One,
+  Two
+}
+
+const main = (part: Part) => (input: string) => {
   const passports = input.split('\n\n').map(parser)
-  return passports.map(validator).filter(Boolean).length
+  return passports.map(validator(part)).filter(Boolean).length
 }
 
-export function validator(passport: Partial<Passport>) {
-  return (
-    !!passport.byr &&
-    !!passport.iyr &&
-    !!passport.eyr &&
-    !!passport.hgt &&
-    !!passport.hcl &&
-    !!passport.ecl &&
-    !!passport.pid
-  )
-}
+export const part1 = main(Part.One)
+export const part2 = main(Part.Two)
 
+const eyeColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+const numberFields = ['byr', 'iyr', 'eyr']
+const isHeightFormats = (format: string) => format === 'cm' || format === 'in'
+
+export function validator(part: Part) {
+  return function(passport: Partial<Passport>) {
+    const isFieldsValid =
+      !!passport.byr &&
+      !!passport.iyr &&
+      !!passport.eyr &&
+      !!passport.hgt &&
+      !!passport.hcl &&
+      !!passport.ecl &&
+      !!passport.pid
+
+    if (!isFieldsValid || part === Part.One) return isFieldsValid
+
+    const verifying = passport as Omit<Passport, 'cid'> &
+      Partial<Pick<Passport, 'cid'>>
+
+    const byr = compare(verifying.byr, 1920, 2002)
+    const iyr = compare(verifying.iyr, 2010, 2020)
+    const eyr = compare(verifying.eyr, 2020, 2030)
+
+    const format = verifying.hgt.slice(-2)
+    const isCm = format === 'cm'
+    const hgt =
+      isHeightFormats(format) &&
+      compare(parseInt(verifying.hgt), isCm ? 150 : 59, isCm ? 193 : 76)
+
+    const matched = verifying.hcl.match(/^#[0-9a-f]{6}$/)
+    const hcl = matched !== null
+    const ecl = eyeColors.includes(verifying.ecl)
+    const pid = !isNaN(verifying.pid) && verifying.pid.length === 9
+    return byr && iyr && eyr && hgt && hcl && ecl && pid
+  }
+}
 type rawField = Record<string, string | number>
 
 export function parser(input: string) {
@@ -34,10 +67,15 @@ export function parser(input: string) {
 
     const field: rawField = {}
 
-    const value = isNaN(rawValue) ? rawValue : Number(rawValue)
+    const isNumber = numberFields.includes(key)
+    const value = isNumber ? Number(rawValue) : rawValue
 
     field[key] = value
 
     return { ...reducing, ...field }
   }, {} as Passport)
+}
+
+function compare(value: number, least: number, most: number) {
+  return least <= value && value <= most
 }
